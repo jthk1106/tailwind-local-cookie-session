83b3fadd1a7fd013c8892585e20bcbda2b1a0e1d
Jay <thk116@gmail.com>
2020-08-20 01:48:39 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  important: true,
+  theme: {
+    extend: {
+      colors: {
+        neonOrange: '#ff9933'
+      }
+    },
+    h1: {
+      textAlign: 'center'
+    },
+  },
+  variants: {},
+  plugins: [],
+}
